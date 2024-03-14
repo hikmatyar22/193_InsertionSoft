6958b7d6c81ff610b9eab17c8a0ef8eadd3299dc
hikmatyar22 <abiq5161@gmail.com>
2024-03-14 11:01:16 +0700

    Membuat Prosedur Insertionsort

--- Prak2Algoritmaaa/Prak2Algoritmaaa.cpp
+++ Prak2Algoritmaaa/Prak2Algoritmaaa.cpp
@@ -26,3 +26,27 @@ void input() {                       // Procedur untuk input
 
 	}
 }
+
+void insertionsort() {                        // Prosedur insertionsort
+	int temp;                                 // Membuat Variabel data temporer atau penyimpanan semantara
+	int i, j;                                 // Membuat variabel j sebagai penanda
+
+	for (i = 1; i <= n - 1; i++) {            // 1. Looping dengan i dimulai dari 1 hingga n-1
+
+		temp = arr[i];                      // 2. Simpan nilai arr[i] ke variabel sementara temp
+
+		j = i - 1;                            // 3. Setting nilai j sama dengan i-1
+
+		while (j >= 0 && arr[j] > temp) {    // 4. Looping while dimana nilai j lebih besar sama dengan 0 dan arr[j] lebih besar daripada temp
+			arr[j + 1] = arr[j];              // 4a. Simpan arr[j] ke dalam variabel arr[j+1]
+			j--;                              // 4b. Decrement nilai j by 1
+		}
+
+		arr[j + 1] = temp;                    // 5. simpan nilai temp ke dalam arr[j+1]
+
+		cout << "\nPass" << i << ": ";        // Output ke layar
+		for (int k = 0; k < n; k++) {         // looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";             // Output ke layar
+		}
+	}
+}
